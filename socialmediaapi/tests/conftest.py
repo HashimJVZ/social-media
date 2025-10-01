84d7fbe240f6c1f82b35bbe348df2e7cdb04701d
HashimJVZ <highbrowdirector@gmail.com>
2025-10-01 16:04:22 +0530

    setup and config pytest

---
+++ socialmediaapi/tests/conftest.py
@@ -0,0 +1,32 @@
+from typing import AsyncGenerator, Generator
+
+import pytest
+from fastapi.testclient import TestClient
+
+from httpx import AsyncClient
+
+from socialmediaapi.main import app
+from socialmediaapi.routers.post import comment_table, post_table
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture()
+def client() -> Generator:
+    yield TestClient(app)
+
+
+@pytest.fixture()
+async def db() -> AsyncGenerator:
+    post_table.clear()
+    comment_table.clear
+    yield
+
+
+@pytest.fixture()
+async def async_client(client) -> AsyncGenerator:
+    async with AsyncClient(app=app, base_url=client.base_url) as ac:
+        yield ac
